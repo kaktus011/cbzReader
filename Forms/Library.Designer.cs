16e4824ee601d8c5b97d174df4688575da82892d
kaktus011 <piwkillpie@gmail.com>
2024-01-20 21:56:24 +0200

    ADDED: ability to restore files from library

--- Forms/Library.Designer.cs
+++ Forms/Library.Designer.cs
@@ -29,11 +29,14 @@
         private void InitializeComponent()
         {
             ImportBtn = new Button();
+            importProgBar = new ProgressBar();
+            importingLbl = new Label();
+            RestoreBtn = new Button();
             SuspendLayout();
             // 
             // ImportBtn
             // 
-            ImportBtn.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ImportBtn.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
             ImportBtn.Location = new Point(12, 12);
             ImportBtn.Name = "ImportBtn";
             ImportBtn.Size = new Size(129, 44);
@@ -42,22 +45,61 @@
             ImportBtn.UseVisualStyleBackColor = true;
             ImportBtn.Click += ImportBtn_Click;
             // 
+            // importProgBar
+            // 
+            importProgBar.Location = new Point(243, 23);
+            importProgBar.Name = "importProgBar";
+            importProgBar.Size = new Size(145, 16);
+            importProgBar.Step = 5;
+            importProgBar.Style = ProgressBarStyle.Continuous;
+            importProgBar.TabIndex = 1;
+            importProgBar.Visible = false;
+            // 
+            // importingLbl
+            // 
+            importingLbl.AutoSize = true;
+            importingLbl.Font = new Font("Segoe UI", 11F);
+            importingLbl.Location = new Point(162, 19);
+            importingLbl.Name = "importingLbl";
+            importingLbl.Size = new Size(78, 20);
+            importingLbl.TabIndex = 2;
+            importingLbl.Text = "Importing:";
+            importingLbl.Visible = false;
+            // 
+            // RestoreBtn
+            // 
+            RestoreBtn.Font = new Font("Segoe UI", 13F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RestoreBtn.Location = new Point(448, 12);
+            RestoreBtn.Name = "RestoreBtn";
+            RestoreBtn.Size = new Size(129, 44);
+            RestoreBtn.TabIndex = 3;
+            RestoreBtn.Text = "Restore";
+            RestoreBtn.UseVisualStyleBackColor = true;
+            RestoreBtn.Click += RestoreBtn_Click;
+            // 
             // Library
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             AutoScroll = true;
             ClientSize = new Size(589, 416);
+            Controls.Add(RestoreBtn);
+            Controls.Add(importingLbl);
+            Controls.Add(importProgBar);
             Controls.Add(ImportBtn);
             DoubleBuffered = true;
             Name = "Library";
             Text = "Library";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button importBtn;
         private Button ImportBtn;
+        private ProgressBar importProgBar;
+        private Label importingLbl;
+        private Button RestoreBtn;
     }
 }
--- Forms/Library.cs
+++ Forms/Library.cs
@@ -1,26 +1,27 @@
+using System;
 using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
+using System.IO;
 using System.IO.Compression;
 
 namespace cbzReader.Forms
 {
     //TODO
-    //adding big files takes a while
-    //restore from library directory
-    //make everything dark mode (easier on the eyes) multiple
-    //progress bar for importing files
     //option for next page going left or next page going right
+    //make everything dark mode (easier on the eyes) multiple
+    //be able to delete a file
+    //TODO in ImportBtn_Click() check if file is already in _comicExtractLocation at the start
 
     //BUG(s)
-    //adding the same file breaks everything
+    //adding big files takes a while
 
 
     public partial class Library : Form
     {
         private readonly List<ComicBook> _books = [];
 
-        private readonly string _comicExtractLocation =
-            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\cbzViewerLib";
+        private readonly string _comicExtractLocation = "C:\\Users\\PiwKi\\Desktop" + @"\cbzViewerLib";
+        //Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\cbzViewerLib";
 
         internal const int PageWidth = 794;
         internal const int PageHeight = 1123;
@@ -43,24 +44,50 @@ namespace cbzReader.Forms
             if (opnFile.ShowDialog() == DialogResult.OK)
             {
                 var path = opnFile.FileName;
+                if (CheckForFileInLib(path))
+                {
+                    MessageBox.Show("Error: File already in library. Click 'Restore' if it isn't shown");
+                    return;
+                }
                 var comic = new ComicBook
                 {
                     Location = path,
                     Title = Path.GetFileName(path).Substring(0, Path.GetFileName(path).IndexOf('.'))
                 };
 
-                if (_books.FirstOrDefault(book => book.Title == comic.Title) == null) 
+                if (_books.FirstOrDefault(book => book.Title == comic.Title) == null)
                     Import(comic);
                 else
                 {
-                    MessageBox.Show("Error: File already imported.");
+                    MessageBox.Show("Error: File already in library.");
                 }
             }
         }
 
+        private void RestoreBtn_Click(object sender, EventArgs e)
+        {
+            ImportOnOpening();
+            RestoreBtn.Visible = false;
+        }
+
+        private bool CheckForFileInLib(string path)
+        {
+            var tmpTitle = path.Substring(path.LastIndexOf("\\") + 1, Path.GetFileName(path).IndexOf('.'));
+            var folderPaths = Directory.EnumerateDirectories(_comicExtractLocation)
+                                                .Where(currPath => currPath.Substring(currPath.LastIndexOf("\\") + 1) == tmpTitle);
+
+            return folderPaths.Count() != 0;
+        }
+
         private void Import(ComicBook comic)
         {
+            importingLbl.Visible = true;
+            importProgBar.Value = 0;
+            importProgBar.Visible = true;
+
             var imgPaths = ExtractComic(comic);
+            importProgBar.Maximum = imgPaths.Length;
+            Refresh();
             comic.Pages = imgPaths.Length;
             var tmp = Image.FromFile(imgPaths[0]);
             comic.Cover = ResizeImage(tmp, 105, 147);
@@ -82,9 +109,70 @@ namespace cbzReader.Forms
             {
                 var img = Image.FromFile(path);
                 comic.Panels.Add(ResizeImage(img, PageWidth, PageHeight));
+                importProgBar.PerformStep();
             }
 
             _books.Add(comic);
+
+            importingLbl.Visible = false;
+            importProgBar.Visible = false;
+        }
+
+        private void ImportOnOpening()
+        {
+            if (!Directory.Exists(_comicExtractLocation))
+                return;
+
+            importingLbl.Visible = true;
+            importProgBar.Value = 0;
+            importProgBar.Visible = true;
+            Refresh();
+
+            var folderPaths = Directory.EnumerateDirectories(_comicExtractLocation).ToArray();
+            importProgBar.Maximum = folderPaths.Length;
+            importProgBar.Step = 1;
+
+            foreach (var fPath in folderPaths)
+            {
+                var imgPaths = Directory.EnumerateFiles(fPath, "*.*", SearchOption.AllDirectories).ToArray();
+
+                //bad name -> returns comic title with '\\' at the beginning
+                var tmpTitle = fPath.Substring(fPath.LastIndexOf("\\"));
+                var newComic = new ComicBook
+                {
+                    Title = tmpTitle.Substring(1),
+                    Location = fPath + tmpTitle,
+                    Cover = ResizeImage(Image.FromFile(imgPaths[0]), 105, 147),
+                    Pages = imgPaths.Length,
+                };
+
+                foreach (var path in imgPaths)
+                {
+                    var img = Image.FromFile(path);
+                    newComic.Panels.Add(ResizeImage(img, PageWidth, PageHeight));
+                }
+
+                var newPicBox = new PictureBox
+                {
+                    Size = new Size(105, 147),
+                    Image = newComic.Cover,
+                    Location = new Point(_coverPosX, _coverPosY),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    Text = newComic.Title,
+                };
+                newPicBox.DoubleClick += (sender, e) => { Read(newPicBox.Text); };
+                Controls.Add(newPicBox);
+
+                importProgBar.PerformStep();
+                CalculateNextCoverPos();
+
+                _books.Add(newComic);
+                Refresh();
+            }
+
+            importingLbl.Visible = false;
+            importProgBar.Visible = false;
+            importProgBar.Step = 5;
         }
 
         private void CalculateNextCoverPos()
